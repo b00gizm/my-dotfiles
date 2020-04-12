7bc241779cc98f2407d34b5204ea1d19b083f7b1
Pascal Cremer <b00gizm@gmail.com>
2020-04-12 13:50:51 +0200

    Add OpenJDK post install steps for macOS

--- macos/init.sh
+++ macos/init.sh
@@ -28,3 +28,6 @@ print_info "📦 Installing apps from ${DIR}/macos/Brewfile…"
 brew update
 brew bundle --file=${DIR}/macos/Brewfile
 brew cleanup
+
+# For Java wrappers to find the JDK
+sudo ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
